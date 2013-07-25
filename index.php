9e599f3485496fe3a6e48c8e9fc6a79eb93d4d02
Sundragoon <a.limanthie@yahoo.com>
2013-07-25 08:27:18 +0700

    Add slide images and finished carousel function

--- index.php
+++ index.php
@@ -15,9 +15,25 @@
 
 		<div id="slides"> 
 			<ul>
-				<li><img src="img/front_image.jpg" alt="Slide 1"/></li>
-				<li><img src="img/front_image.jpg" alt="Slide 2"/></li>
-				<li><img src="img/front_image.jpg" alt="Slide 3"/></li>
+				<li><img src="img/Slide 1.jpg" alt="Slide 1"/></li>
+				<li><img src="img/Slide 2.jpg" alt="Slide 2"/></li>
+				<li><img src="img/Slide 3.jpg" alt="Slide 3"/></li>
+				<li><img src="img/Slide 4.jpg" alt="Slide 4"/></li>
+				<li><img src="img/Slide 5.jpg" alt="Slide 5"/></li>
+				<li><img src="img/Slide 6.jpg" alt="Slide 6"/></li>
+				<li><img src="img/Slide 7.jpg" alt="Slide 7"/></li>
+				<li><img src="img/Slide 8.jpg" alt="Slide 8"/></li>
+				<li><img src="img/Slide 9.jpg" alt="Slide 9"/></li>
+				<li><img src="img/Slide 10.jpg" alt="Slide 10"/></li>
+				<li><img src="img/Slide 11.jpg" alt="Slide 11"/></li>
+				<li><img src="img/Slide 12.jpg" alt="Slide 12"/></li>
+				<li><img src="img/Slide 13.jpg" alt="Slide 13"/></li>
+				<li><img src="img/Slide 14.jpg" alt="Slide 14"/></li>
+				<li><img src="img/Slide 15.jpg" alt="Slide 15"/></li>
+				<li><img src="img/Slide 16.jpg" alt="Slide 16"/></li>
+				<li><img src="img/Slide 17.jpg" alt="Slide 17"/></li>
+				<li><img src="img/Slide 18.jpg" alt="Slide 18"/></li>
+				<li><img src="img/Slide 19.jpg" alt="Slide 19"/></li>
 			</ul>
 		</div>
 
